ecc8f244164a20d8dae5bd2ae2a0c06bcad07973
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2011-12-14 09:23:32 +1300

    Minor display fix for "blog-like" forums (overlapping text)

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2011120701; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2011120702; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2010112400; // Requires this Moodle version
 $plugin->component = 'theme_decaf';
 $plugin->maturity  = MATURITY_STABLE;

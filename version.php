cff1d47d9f348bd57e922378ec0735dfbf847dc6
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2012-05-03 16:23:25 +1200

    Add missing semicolon in version.php

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2012043001; // The current module version (Date: YYYYMMDDXX)
-$plugin->release   = "1.5 RC1"
+$plugin->release   = "1.5 RC1";
 $plugin->requires  = 2010112400; // Requires this Moodle version
 $plugin->component = 'theme_decaf';
 $plugin->maturity  = MATURITY_RC;

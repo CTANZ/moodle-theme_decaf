5c612b3941dc214dad5360dfd0f94cd182d03e69
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2012-05-03 16:20:11 +1200

    Version bump / maturity change (now RC)

--- version.php
+++ version.php
@@ -1,7 +1,8 @@
 <?php
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2012043000; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2012043001; // The current module version (Date: YYYYMMDDXX)
+$plugin->release   = "1.5 RC1"
 $plugin->requires  = 2010112400; // Requires this Moodle version
 $plugin->component = 'theme_decaf';
-$plugin->maturity  = MATURITY_STABLE;
+$plugin->maturity  = MATURITY_RC;

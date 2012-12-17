362e4125e04f3c3ac541b7c3739387c20b4160bc
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2012-12-18 11:10:10 +1300

    Version bump to 1.6.10

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2012101709; // The current module version (Date: YYYYMMDDXX)
-$plugin->release   = "1.6.9";
+$plugin->version   = 2012101710; // The current module version (Date: YYYYMMDDXX)
+$plugin->release   = "1.6.10";
 $plugin->requires  = 2010112400; // Requires this Moodle version
 $plugin->component = 'theme_decaf';
 $plugin->maturity  = MATURITY_STABLE;

f958b699522df47f1527832c527c74229d7508cc
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2013-01-31 14:21:47 +1300

    Version bump to 1.7
    Also bumps minimum required Moodle version to 2.3.

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2012101710; // The current module version (Date: YYYYMMDDXX)
-$plugin->release   = "1.6.10";
-$plugin->requires  = 2010112400; // Requires this Moodle version
+$plugin->version   = 2013013100; // The current module version (Date: YYYYMMDDXX)
+$plugin->release   = "1.7";
+$plugin->requires  = 2012062500; // Requires this Moodle version (2.3)
 $plugin->component = 'theme_decaf';
 $plugin->maturity  = MATURITY_STABLE;

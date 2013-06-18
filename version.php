1a79dd470fe89260b1fdcb393ad6d4b647bac8bd
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2013-06-19 09:51:12 +1200

    Version bump, now requires Moodle 2.4
    No release name update as it has not yet been released.

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2013061800; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2013061900; // The current module version (Date: YYYYMMDDXX)
 $plugin->release   = "1.8.0";
-$plugin->requires  = 2012062500; // Requires this Moodle version (2.3)
+$plugin->requires  = 2012120300; // Requires this Moodle version (2.3)
 $plugin->component = 'theme_decaf';
 $plugin->maturity  = MATURITY_STABLE;

CTANZ/moodle-theme_decaf

commit e83f40d9c9a97d62b6e340f5dcd7e3ed448f4ca8

Further 2.6 compat
Adds support for navigation_node::TYPE_MY_CATEGORY and implements a check
before expanding subnav to prevent duplication of nodes.

lib.php
@@ -443,9 +443,11 @@ class decaf_expand_navigation extends global_navigation {
     private $expandtocourses = true;
     private $expandedcourses = array();
 
-    // Added in 2.6, so we need to specify it here so that earlier versions don't complain.
+    // Added in 2.6, so we need to specify these here so that earlier versions don't complain.
     /** @var int site admin branch node type, used only within settings nav 71 */
     const TYPE_SITE_ADMIN = 71;
+    /** var int Category displayed in MyHome navigation node */
+    const TYPE_MY_CATEGORY = 11;
 
     /**
      * Constructs the navigation for use in AJAX request
@@ -525,6 +527,7 @@ class decaf_expand_navigation extends global_navigation {
                 }
                 break;
             case self::TYPE_CATEGORY :
+            case self::TYPE_MY_CATEGORY :
                 if (!empty($PAGE->theme->settings->coursesleafonly)) {
                     return false;
                 }

renderers.php
@@ -340,14 +340,17 @@ class theme_decaf_topsettings_renderer extends plugin_renderer_base {
                     }
                     $subnav = $mainsubnav;
                 }
-                if (!$expanded) {
-                    // re-use subnav so we don't have to reinitialise everything
-                    $subnav->expand($item->type, $item->key);
-                }
-                if (!isloggedin() || isguestuser()) {
-                    $subnav->set_expansion_limit(navigation_node::TYPE_COURSE);
-                }
                 $branch = $subnav->find($item->key, $item->type);
+                if ($branch === false) {
+                    if (!$expanded) {
+                        // re-use subnav so we don't have to reinitialise everything
+                        $subnav->expand($item->type, $item->key);
+                    }
+                    if (!isloggedin() || isguestuser()) {
+                        $subnav->set_expansion_limit(navigation_node::TYPE_COURSE);
+                    }
+                    $branch = $subnav->find($item->key, $item->type);
+                }
                 if($branch!==false) $content .= $this->navigation_node($branch);
             } else {
                 $content .= $this->navigation_node($item);

commit 84037c2c7106c504b6b3000c08dcd8c1bd5f6445

2.6 compat redux - restore missing course branches
Partially reverts e83f40d9, which caused these branches to go missing.
This change should still prevent warnings about adding nodes that already
exist.

lib.php
@@ -805,6 +805,9 @@ class decaf_expand_navigation extends global_navigation {
             $coursetype = self::COURSE_CURRENT;
         }
         if ($this->expandtocourses || $coursetype == self::COURSE_MY || $coursetype == self::COURSE_CURRENT) {
+            if ($coursenode = $this->find($course->id, self::TYPE_COURSE)) {
+                return $coursenode;
+            }
             return parent::add_course($course, $forcegeneric, $coursetype);
         }
         return false;

renderers.php
@@ -340,17 +340,14 @@ class theme_decaf_topsettings_renderer extends plugin_renderer_base {
                     }
                     $subnav = $mainsubnav;
                 }
-                $branch = $subnav->find($item->key, $item->type);
-                if ($branch === false) {
-                    if (!$expanded) {
-                        // re-use subnav so we don't have to reinitialise everything
-                        $subnav->expand($item->type, $item->key);
-                    }
-                    if (!isloggedin() || isguestuser()) {
-                        $subnav->set_expansion_limit(navigation_node::TYPE_COURSE);
-                    }
-                    $branch = $subnav->find($item->key, $item->type);
+                if (!$expanded) {
+                    // re-use subnav so we don't have to reinitialise everything
+                    $subnav->expand($item->type, $item->key);
                 }
+                if (!isloggedin() || isguestuser()) {
+                    $subnav->set_expansion_limit(navigation_node::TYPE_COURSE);
+                }
+                $branch = $subnav->find($item->key, $item->type);
                 if($branch!==false) $content .= $this->navigation_node($branch);
             } else {
                 $content .= $this->navigation_node($item);
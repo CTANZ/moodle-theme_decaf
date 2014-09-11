2c6b2c640ead0224f53ef916477f17b9488a8f9d
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2014-09-12 11:31:36 +1200

    Override core_course_renderer to handle persistent editing mode
    core_course_renderer::course_section_cm_list_item() is called by the file
    dragdrop handler to generate the new module instance for the course, but
    it didn't know about persistent editing mode so no editing controls were
    generated if "real editing mode" wasn't enabled.

---
+++ classes/core_course_renderer.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Core course renderer for Decaf.
+ *
+ * @package    theme_decaf
+ * @copyright  2014 Paul Nicholls
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class theme_decaf_core_course_renderer extends core_course_renderer {
+
+    /**
+     * Renders HTML to display one course module for display within a section.
+     *
+     * This function calls:
+     * {@link core_course_renderer::course_section_cm()}
+     *
+     * @param stdClass $course
+     * @param completion_info $completioninfo
+     * @param cm_info $mod
+     * @param int|null $sectionreturn
+     * @param array $displayoptions
+     * @return String
+     */
+    public function course_section_cm_list_item($course, &$completioninfo, cm_info $mod, $sectionreturn, $displayoptions = array()) {
+        global $PAGE, $USER;
+
+        // Enable editing if we're in persistent editing mode and not "really editing".
+        $wasediting = $USER->editing;
+        if (!empty($this->page->theme->settings->persistentedit) && !$USER->editing) {
+            $USER->editing = 1;
+        }
+
+        $output = parent::course_section_cm_list_item($course, $completioninfo, $mod, $sectionreturn, $displayoptions);
+
+        // Set editing flag back to what it was before we did this.
+        $USER->editing = $wasediting;
+
+        return $output;
+    }
+}

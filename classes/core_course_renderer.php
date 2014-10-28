2df21360fc4d69b5308f1781b76233dd1cc262f8
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2014-10-29 11:52:59 +1300

    modchooser tiles: Use background image for IE8 compat
    IE8 doesn't select the radio button when clicking on an image inside the
    label, so we use a background image on the span instead.  That way the
    double-click still works in IE8.

--- classes/core_course_renderer.php
+++ classes/core_course_renderer.php
@@ -40,4 +40,54 @@ class theme_decaf_core_course_renderer extends core_course_renderer {
 
         return $output;
     }
+
+    /**
+     * Return the HTML for the specified module adding any required classes
+     *
+     * @param object $module An object containing the title, and link. An
+     * icon, and help text may optionally be specified. If the module
+     * contains subtypes in the types option, then these will also be
+     * displayed.
+     * @param array $classes Additional classes to add to the encompassing
+     * div element
+     * @return string The composed HTML for the module
+     */
+    protected function course_modchooser_module($module, $classes = array('option')) {
+        $output = '';
+        $output .= html_writer::start_tag('div', array('class' => implode(' ', $classes)));
+        $output .= html_writer::start_tag('label', array('for' => 'module_' . $module->name));
+        if (!isset($module->types)) {
+            $output .= html_writer::tag('input', '', array('type' => 'radio',
+                    'name' => 'jumplink', 'id' => 'module_' . $module->name, 'value' => $module->link));
+        }
+
+        $attributes = array('class' => 'modicon');
+        if (isset($module->icon)) {
+            // Add an icon if we have one
+            $attributes['style'] = 'background-image:url('.$this->pix_url('icon', $module->name).');';
+        }
+        $output .= html_writer::tag('span', '', $attributes);
+
+        $output .= html_writer::tag('span', $module->title, array('class' => 'typename'));
+        if (!isset($module->help)) {
+            // Add help if found
+            $module->help = get_string('nohelpforactivityorresource', 'moodle');
+        }
+
+        // Format the help text using markdown with the following options
+        $options = new stdClass();
+        $options->trusted = false;
+        $options->noclean = false;
+        $options->smiley = false;
+        $options->filter = false;
+        $options->para = true;
+        $options->newlines = false;
+        $options->overflowdiv = false;
+        $module->help = format_text($module->help, FORMAT_MARKDOWN, $options);
+        $output .= html_writer::tag('span', $module->help, array('class' => 'typesummary'));
+        $output .= html_writer::end_tag('label');
+        $output .= html_writer::end_tag('div');
+
+        return $output;
+    }
 }
